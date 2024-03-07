2ad7e482227af902f60d897f44b627963b9d0c7b
pdkkg <kushwahagautam24@gmail.com>
2024-03-07 14:59:38 +0530

    added

--- app/home/services.jsx
+++ app/home/services.jsx
@@ -1,19 +1,50 @@
-// CartAnimation.jsx
 import React from 'react';
-import Image from 'next/image';
-// import styles from './CartAnimation.module.css'; // Import the CSS file
-import style from "@/styles/CartAnimation.module.css"
-const CartAnimation = () => {
+
+export default function ServiceSection() {
   return (
-    <div className={style['cart-item']}>
-      <div className={style.content}>
-        Content goes here. This could be the item description or other details.
+    <section className="pb-100 py-12 ml-50 ">
+      <div className="container mx-auto">
+        <h2 className="text-4xl font-bold text-center mb-8 text-gray-700">We Expertise On This Field</h2>
+        <p className="text-gray-600 mt-2 px-50 w-[40%] text-center mx-auto pb-10">
+        More than just services, we're your partner. We collaborate with you to understand your unique needs and deliver customized solutions that drive real results.</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-40 ">
+          {/* Service Card 1 */}
+          <div className="bg-[#0079A0] py-12 rounded-lg flex flex-col items-center px-5">
+            <img src="https://img.freepik.com/premium-vector/handshake-icon-friendship-sign-partnership-symbol-hand-shake-vector-icon_564974-1217.jpg?w=1060" alt="Service 1" className="w-24 h-24 my-10 text-center" />
+            <h3 className="text-4xl text-white font-extrabold text-center">Consultation</h3>
+          </div>
+
+          {/* Service Card 2 */}
+          <div className="bg-[#0079A0] py-12 rounded-lg flex flex-col items-center px-5">
+            <img src="https://img.freepik.com/premium-vector/handshake-icon-friendship-sign-partnership-symbol-hand-shake-vector-icon_564974-1217.jpg?w=1060" alt="Service 1" className="w-24 h-24 my-10 text-center" />
+            <h3 className="text-4xl text-white font-extrabold text-center">Consultation</h3>
+          </div>
+
+          {/* Service Card 3 */}
+          <div className="bg-[#0079A0] py-12 rounded-lg flex flex-col items-center px-5">
+            <img src="https://img.freepik.com/premium-vector/handshake-icon-friendship-sign-partnership-symbol-hand-shake-vector-icon_564974-1217.jpg?w=1060" alt="Service 1" className="w-24 h-24 my-10 text-center" />
+            <h3 className="text-4xl text-white font-extrabold text-center">Consultation</h3>
+          </div>
+
+          {/* Service Card 4 */}
+          <div className="bg-[#0079A0] py-12 rounded-lg flex flex-col items-center px-5">
+            <img src="https://img.freepik.com/premium-vector/handshake-icon-friendship-sign-partnership-symbol-hand-shake-vector-icon_564974-1217.jpg?w=1060" alt="Service 1" className="w-24 h-24 my-10 text-center" />
+            <h3 className="text-4xl text-white font-extrabold text-center">Consultation</h3>
+          </div>
+
+          {/* Service Card 5 */}
+          <div className="bg-[#0079A0] py-12 rounded-lg flex flex-col items-center px-5">
+            <img src="https://img.freepik.com/premium-vector/handshake-icon-friendship-sign-partnership-symbol-hand-shake-vector-icon_564974-1217.jpg?w=1060" alt="Service 1" className="w-24 h-24 my-10 text-center" />
+            <h3 className="text-4xl text-white font-extrabold text-center">Consultation</h3>
+          </div>
+
+          {/* Service Card 6 */}
+          <div className="bg-[#0079A0] py-12 rounded-lg flex flex-col items-center px-5">
+            <img src="https://img.freepik.com/premium-vector/handshake-icon-friendship-sign-partnership-symbol-hand-shake-vector-icon_564974-1217.jpg?w=1060" alt="Service 1" className="w-24 h-24 my-10 text-center" />
+            <h3 className="text-4xl text-white font-extrabold text-center">Consultation</h3>
+          </div>
+        </div>
       </div>
-      <div className={style.background}></div>
-      <Image src="/hands.png" alt="client" width={200} height={200} className='bg-black' />
-      <p>consultancy</p>
-    </div>
+    </section>
   );
 };
-
-export default CartAnimation;
